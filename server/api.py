162762be005a85c9044dd45db208b14161c8e6cd
Lex <u16ak17@abdn.ac.uk>
2023-04-03 12:32:34 +0100

    Add add_drop and add_shift to API

--- server/api.py
+++ server/api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import List, Tuple
 import game_logic
 
 
@@ -7,5 +8,13 @@ game = game_logic.Game()
 
 
 @app.get('/board')
-def board():
+async def board():
     return {'board': game.board}
+
+@app.post("/add-drop")
+async def add_drop(x: int, y: int):
+    game.add_drop(int(x),int(y))
+
+@app.post("/add_shift")
+async def add_shift(n, d):
+    game.add_shift(int(n), [int(x) for x in d.split(",")])
