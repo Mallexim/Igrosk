e605a183f4ee99f46e5377148def35cc9e86ca73
Lex <u16ak17@abdn.ac.uk>
2023-04-04 13:01:24 +0100

    More work on the FastAPI, comments

--- server/api.py
+++ server/api.py
@@ -10,7 +10,7 @@ import game_logic
 app = FastAPI()
 
 rooms = {}
-game = game_logic.Game()
+# game = game_logic.Game()
 
 
 class Room(BaseModel):
@@ -50,36 +50,28 @@ async def join_room(websocket: WebSocket, room_id: str):
         success = await room.add_player(websocket)
         if success:
             try:
-                # Something here
-                return {"message": f"Joined room {room_id}"}
+                while True:
+                    # Receive data from player's WebSocket connection
+                    data = await websocket.receive_text()
+                    # Parse data
+                    # Update game state
+                    # Broadcast new game state to all players in room
             except:
                 room.connections.remove(websocket)
-
     else:
         await websocket.close(code=1008)
 
 
-@app.websocket("/game/{room_id}/{player_id}")
-async def ws_endpoint(websocket: WebSocket, room_id: str, player_id: str):
-    await websocket.accept()
-    if room_id not in rooms:
-        rooms[room_id] = game_logic.Game()
-    if player_id == 'white':
-        rooms[room_id].white_ws = websocket
-    else:
-        rooms[room_id].black_ws = websocket
+# @app.get('/board')
+# async def board():
+#     return {'board': game.board}
 
 
-@app.get('/board')
-async def board():
-    return {'board': game.board}
+# @app.post("/add-drop")
+# async def add_drop(x: int, y: int):
+#     game.add_drop(int(x), int(y))
 
 
-@app.post("/add-drop")
-async def add_drop(x: int, y: int):
-    game.add_drop(int(x), int(y))
-
-
-@app.post("/add_shift")
-async def add_shift(n: int, d: List):
-    game.add_shift(n, d)
+# @app.post("/add_shift")
+# async def add_shift(n: int, d: List):
+#     game.add_shift(n, d)
