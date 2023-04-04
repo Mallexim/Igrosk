94a65b264c402fc3ba0afae301a22b4241e2965c
Lex <u16ak17@abdn.ac.uk>
2023-04-04 10:57:21 +0100

    Use pydantic

--- server/api.py
+++ server/api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
 
 from typing import List, Tuple
 import game_logic
@@ -10,14 +11,13 @@ app = FastAPI()
 rooms = {}
 game = game_logic.Game()
 
-class Room:
-    def __init__(self, room_id):
-        self.room_id = room_id
-        self.game = game_logic.Game()
+class Room(BaseModel):
+    room_id: str
+    game: game_logic.Game
 
-        # Websocket addresses for players
-        self.white_ws = None
-        self.black_ws = None
+    # Websocket addresses for players
+    white_ws: WebSocket = None
+    black_ws: WebSocket = None
 
 @app.websocket("/game/{game_id}/{player_id}")
 async def ws_endpoint(websocket: WebSocket, room_id: str, player_id: str):
@@ -38,5 +38,5 @@ async def add_drop(x: int, y: int):
     game.add_drop(int(x),int(y))
 
 @app.post("/add_shift")
-async def add_shift(n, d):
-    game.add_shift(int(n), [int(x) for x in d.split(",")])
+async def add_shift(n: int, d: List):
+    game.add_shift(n, d)
